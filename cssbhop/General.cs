5b7e0b4b4d5b2734c614759427b8135e6af38667
shavitush <djshavit@gmail.com>
2016-10-02 10:19:00 +0300

    Add auto-updating functionality.

---
+++ cssbhop/General.cs
@@ -0,0 +1,21 @@
+﻿namespace cssbhop
+{
+	public class General
+	{
+		/// <summary>
+		/// Private variables.
+		/// </summary>
+		private static float fVersion = 1.2f;
+
+		/// <summary>
+		/// Retrieves the current version of cssbhop.
+		/// </summary>
+		public static float Version
+		{
+			get
+			{
+				return fVersion;
+			}
+		}
+	}
+}
