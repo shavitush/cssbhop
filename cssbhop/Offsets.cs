6f088011e0745f0f9df8c347632bd64f7b2bdc4a
shavitush <djshavit@gmail.com>
2016-11-02 21:12:00 +0200

    Remove MoveTypes enumerator

--- cssbhop/Offsets.cs
+++ cssbhop/Offsets.cs
@@ -6,25 +6,6 @@
 	/// </summary>
 	internal class Offsets
 	{
-		#region Source Engine enumerators
-		// Taken from SourceMod (addons/sourcemod/scripting/include/entity_prop_stocks.inc).
-		public enum MoveTypes
-		{
-			MovetypeNone = 0,          /**< never moves */
-			MovetypeIsometric,         /**< For players */
-			MovetypeWalk,              /**< Player only - moving on the ground */
-			MovetypeStep,              /**< gravity, special edge handling -- monsters use this */
-			MovetypeFly,               /**< No gravity, but still collides with stuff */
-			MovetypeFlygravity,        /**< flies through the air + is affected by gravity */
-			MovetypeVphysics,          /**< uses VPHYSICS for simulation */
-			MovetypePush,              /**< no clip to world, push and crush */
-			MovetypeNoclip,            /**< No gravity, no collisions, still do velocity/avelocity */
-			MovetypeLadder,            /**< Used by players only when going onto a ladder */
-			MovetypeObserver,          /**< Observer movement, depends on player's observer mode */
-			MovetypeCustom             /**< Allows the entity to describe its own physics */
-		}
-		#endregion
-
 		#region Memory addresses
 		/// <summary>
 		/// client.dll + this is the player struct.
